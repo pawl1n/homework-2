dc42c33bf8b49c1d0df04aeda627ca4c05d1f4e1
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-10-15 17:57:32 +0300

    feat: initializing array with random values, print only even numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int[] arr = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(10);
+        }
+
+        for (int number : arr) {
+            if (number %2 == 0) {
+                System.out.println(number);
+            }
+        }
     }
 }
